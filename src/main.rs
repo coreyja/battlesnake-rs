ebd84eecc9ca89f7c05259a60c8ff764b469435f
Corey Alexander <coreyja@gmail.com>
2021-02-20 14:49:33 -0500

    Get the about me route done, now to gameplay!

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,33 @@
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate serde_derive;
+
+use rocket_contrib::json::Json;
+
+#[derive(Serialize)]
+struct AboutMe {
+    apiversion: String,
+    author: Option<String>,
+    color: Option<String>,
+    head: Option<String>,
+    tail: Option<String>,
+    version: Option<String>,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn me() -> Json<AboutMe> {
+    Json(AboutMe {
+        apiversion: "1".to_owned(),
+        author: Some("coreyja".to_owned()),
+        color: Some("#AA66CC".to_owned()),
+        head: None,
+        tail: None,
+        version: None,
+    })
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![me]).launch();
 }
