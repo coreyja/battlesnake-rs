c54527622747d5250d6ee82a6ecaf8c0a292b092
Corey Alexander <coreyja@gmail.com>
2021-02-20 15:39:18 -0500

    Add a hard coded move and start routes

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate rocket;
 #[macro_use]
 extern crate serde_derive;
 
+use rocket::http::Status;
 use rocket_contrib::json::Json;
 
 #[derive(Serialize)]
@@ -29,6 +30,78 @@ fn me() -> Json<AboutMe> {
     })
 }
 
+#[derive(Deserialize, Debug)]
+struct Ruleset {
+    name: String,
+    version: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Game {
+    id: String,
+    ruleset: Option<Ruleset>,
+    timeout: u64,
+}
+
+#[derive(Deserialize, Debug)]
+struct Coordinate {
+    x: u64,
+    y: u64,
+}
+
+#[derive(Deserialize, Debug)]
+struct Battlesnake {
+    id: String,
+    name: String,
+    health: u8,
+    body: Vec<Coordinate>,
+    latency: u16,
+    head: Coordinate,
+    length: u64,
+    shout: Option<String>,
+    squad: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Board {
+    height: u64,
+    width: u64,
+    food: Vec<Coordinate>,
+    hazards: Vec<Coordinate>,
+    snakes: Vec<Battlesnake>,
+}
+
+#[derive(Deserialize, Debug)]
+struct GameState {
+    game: Game,
+    turn: u64,
+    board: Board,
+    you: Battlesnake,
+}
+
+#[post("/start", data = "<game_state>")]
+fn start(game_state: Json<GameState>) -> Status {
+    println!("{:?}", game_state);
+    Status::NoContent
+}
+
+#[derive(Serialize, Debug)]
+struct MoveOutput {
+    r#move: String,
+    shout: String,
+}
+
+#[post("/move", data = "<game_state>")]
+fn api_move(game_state: Json<GameState>) -> Json<MoveOutput> {
+    println!("{:?}", game_state);
+    Json(MoveOutput {
+        r#move: "down".to_owned(),
+        shout: "".to_owned(),
+    })
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![me]).launch();
+    rocket::ignite()
+        .mount("/", routes![me, start, api_move])
+        .launch();
 }
