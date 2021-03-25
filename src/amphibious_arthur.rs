25c0e43b1a3196d2570d9872608c08ad8aed13e6
Corey Alexander <coreyja@gmail.com>
2021-03-24 21:11:40 -0400

    Try to make arthur fail less head to head by avoiding the heads of other snakes. It does so by adding any possible direction the snake could take at each level down. Each time it picks a random 'new head' from the spawned coords added to the body

--- src/amphibious_arthur.rs
+++ src/amphibious_arthur.rs
@@ -24,6 +24,39 @@ pub fn me() -> Json<AboutMe> {
     })
 }
 
+trait MoveToAndSpawn {
+    fn move_to_and_opponent_sprawl(&self, coor: &Coordinate) -> Self;
+}
+
+use rand::seq::SliceRandom;
+
+impl MoveToAndSpawn for GameState {
+    fn move_to_and_opponent_sprawl(&self, coor: &Coordinate) -> Self {
+        let mut cloned = self.move_to(coor, &self.you.id);
+
+        let mut opponents: Vec<&mut Battlesnake> = cloned
+            .board
+            .snakes
+            .iter_mut()
+            .filter(|s| s.id == self.you.id)
+            .collect();
+
+        for s in opponents.iter_mut() {
+            let mut new_body: Vec<Coordinate> = s
+                .head
+                .possbile_moves(&self.board)
+                .iter()
+                .map(|(_dir, coor)| coor)
+                .cloned()
+                .collect();
+            s.head = new_body.choose(&mut rand::thread_rng()).unwrap().clone();
+            s.body.append(&mut new_body);
+        }
+
+        cloned
+    }
+}
+
 fn score(game_state: &GameState, coor: &Coordinate, times_to_recurse: u8) -> i64 {
     const PREFERRED_HEALTH: i64 = 80;
 
@@ -57,7 +90,7 @@ fn score(game_state: &GameState, coor: &Coordinate, times_to_recurse: u8) -> i64
         .iter()
         .map(|(_d, c)| {
             score(
-                &game_state.move_to(coor, &game_state.you.id),
+                &game_state.move_to_and_opponent_sprawl(coor),
                 &c,
                 times_to_recurse - 1,
             )
