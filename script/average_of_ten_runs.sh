3f6cebd8fda413f9b685b27e0a24dcfbd6ef5dd5
Corey Alexander <coreyja@gmail.com>
2021-04-26 21:18:10 -0400

    Fix the existing script and add a new ruby one to make arthur and devin fight

--- script/average_of_ten_runs.sh
+++ script/average_of_ten_runs.sh
@@ -6,7 +6,7 @@ sumss=0
 for i in $(seq $NUMBER_OF_RUNS)  # you can also use {0..9}
 do
   declare -i thisRun
-  thisRun=$(battlesnake play -g solo -n 'test' -u "http://localhost:8000" -H 7 -W 7 |& grep 'DONE' | sed -n "s/.*Game completed after \(.*\) turn.*/\1/p")
+  thisRun=$(battlesnake play -g solo -n 'test' -u "http://localhost:8000/amphibious-arthur" -H 7 -W 7 |& grep 'DONE' | sed -n "s/.*Game completed after \(.*\) turn.*/\1/p")
   echo "Run $i: $thisRun"
   sumss=$((sumss+thisRun))
 done
---
+++ script/duels.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+BASE_URL = 'http://localhost:8000'.freeze
+RUNS = 100
+
+CLI_RESULT_REGEX = /after (.*) turns\. (.*) is the winner/.freeze
+
+class Snake
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def url
+    "#{BASE_URL}/#{name}"
+  end
+end
+
+snake_names = %w[amphibious-arthur devious-devin]
+snakes = snake_names.map { |n| Snake.new(n) }
+
+snake_args = snakes.map { |s| "-n #{s.name} -u #{s.url}" }.join ' '
+
+wins = {}
+
+(0...RUNS).each do |i|
+  run_result = `battlesnake play #{snake_args} -H 11 -W 11 2>&1 >/dev/null | tail -n1`
+  match = CLI_RESULT_REGEX.match(run_result)
+  if match
+    turns, winning_name = match.captures
+
+    puts "#{winning_name} won round #{i} in #{turns} turns"
+
+    wins[winning_name] ||= []
+    wins[winning_name] << turns
+  else
+    puts "Round #{i} was a draw"
+  end
+end
+
+total_runs = wins.values.flatten.count
+
+big_winner, winning_turns = wins.max_by { |_, v| v.count }
+winning_pct = winning_turns.count.to_f / total_runs * 100.0
+
+puts "The big winner is ... #{big_winner}! They won #{winning_pct}% of the rounds"
+
+# puts "FullResults\n#{wins.to_yaml}"
