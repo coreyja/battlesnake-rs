43f595daabddb2bee92f05bc6b5d7628efa947c0
Corey Alexander <coreyja@gmail.com>
2021-04-28 10:20:52 -0400

    Duel script can trab interupts now

--- script/duels.rb
+++ script/duels.rb
@@ -3,7 +3,7 @@
 require 'yaml'
 
 BASE_URL = 'http://localhost:8000'.freeze
-RUNS = 100
+RUNS = 1000
 
 CLI_RESULT_REGEX = /after (.*) turns\. (.*) is the winner/.freeze
 
@@ -26,6 +26,20 @@ snake_args = snakes.map { |s| "-n #{s.name} -u #{s.url}" }.join ' '
 
 wins = {}
 
+def print_output(wins)
+  total_runs = wins.values.flatten.count
+
+  big_winner, winning_turns = wins.max_by { |_, v| v.count }
+  winning_pct = winning_turns.count.to_f / total_runs * 100.0
+
+  puts "The big winner is ... #{big_winner}! They won #{winning_pct}% of the rounds (out of #{total_runs} total non draw rounds)"
+end
+
+trap("SIGINT") do
+  print_output(wins)
+  exit!
+end
+
 (0...RUNS).each do |i|
   run_result = `battlesnake play #{snake_args} -H 11 -W 11 2>&1 >/dev/null | tail -n1`
   match = CLI_RESULT_REGEX.match(run_result)
@@ -41,11 +55,6 @@ wins = {}
   end
 end
 
-total_runs = wins.values.flatten.count
-
-big_winner, winning_turns = wins.max_by { |_, v| v.count }
-winning_pct = winning_turns.count.to_f / total_runs * 100.0
-
-puts "The big winner is ... #{big_winner}! They won #{winning_pct}% of the rounds"
+print_output wins
 
 # puts "FullResults\n#{wins.to_yaml}"
