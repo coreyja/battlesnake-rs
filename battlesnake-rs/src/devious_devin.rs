f514573cbdd38a830be5c00837e8f6f1e6b7843f
Corey Alexander <coreyja@gmail.com>
2021-05-22 17:14:45 -0400

    Score returns a fancy enum now

--- battlesnake-rs/src/devious_devin.rs
+++ battlesnake-rs/src/devious_devin.rs
@@ -1,14 +1,14 @@
 use super::*;
 
 use debug_print::debug_println;
-use serde_json::to_value;
+use itertools::Itertools;
 
 pub struct DeviousDevin {}
 
 #[derive(Serialize)]
 pub struct MoveOption {
     moves: Vec<SnakeMove>,
-    score: i64,
+    score: ScoreEndState,
 }
 
 #[derive(Serialize)]
@@ -22,12 +22,22 @@ impl DeviousDevin {
         game_state: GameState,
     ) -> Result<EvaluateOutput, Box<dyn std::error::Error + Send + Sync>> {
         let mut game_state = game_state;
-        let options = minimax_options(&mut game_state, 0, true, i64::MIN, i64::MAX, vec![]);
+        let options = minimax_options(
+            &mut game_state,
+            0,
+            true,
+            WORT_POSSIBLE_SCORE_STATE,
+            BEST_POSSIBLE_SCORE_STATE,
+            vec![],
+        );
 
-        let options: Vec<MoveOption> = options
+        let mut options: Vec<MoveOption> = options
             .into_iter()
             .map(|(score, moves)| MoveOption { score, moves })
             .collect();
+
+        options.sort_by_key(|option| option.score);
+        options.reverse();
 
         Ok(EvaluateOutput { options })
     }
@@ -39,9 +49,16 @@ impl BattlesnakeAI for DeviousDevin {
         game_state: GameState,
     ) -> Result<MoveOutput, Box<dyn std::error::Error + Send + Sync>> {
         let mut game_state = game_state;
-        let (score, moves) = minimax(&mut game_state, 0, true, i64::MIN, i64::MAX, vec![]);
+        let (score, moves) = minimax(
+            &mut game_state,
+            0,
+            true,
+            WORT_POSSIBLE_SCORE_STATE,
+            BEST_POSSIBLE_SCORE_STATE,
+            vec![],
+        );
         debug_println!(
-            "Turn: {} Score: {} Dir: {:?}",
+            "Turn: {} Score: {:?} Dir: {:?}",
             game_state.turn,
             score,
             moves
@@ -71,7 +88,22 @@ impl BattlesnakeAI for DeviousDevin {
 
 const MAX_DEPTH: i64 = 14;
 
-fn score(node: &GameState, depth: i64) -> Option<i64> {
+#[derive(Serialize, PartialEq, PartialOrd, Ord, Eq, Debug, Copy, Clone)]
+enum ScoreEndState {
+    /// depth: i64
+    Lose(i64),
+    /// depth: i64, distance_to_nearest_food: Option<i64>
+    ShorterThanOpponent(i64, Option<i64>),
+    /// depth: i64, distance_to_opponent: Option<i64>
+    LongerThanOpponent(i64, Option<i64>),
+    /// depth: i64
+    Win(i64),
+}
+
+const BEST_POSSIBLE_SCORE_STATE: ScoreEndState = ScoreEndState::Win(i64::MAX);
+const WORT_POSSIBLE_SCORE_STATE: ScoreEndState = ScoreEndState::Lose(i64::MIN);
+
+fn score(node: &GameState, depth: i64) -> Option<ScoreEndState> {
     let me: &Battlesnake = node
         .board
         .snakes
@@ -90,19 +122,19 @@ fn score(node: &GameState, depth: i64) -> Option<i64> {
     let other_body: HashSet<Coordinate> = HashSet::from_iter(not_me.body.iter().cloned());
 
     if other_body.contains(&me.body[0]) {
-        return Some(SCORE_LOSE + depth);
+        return Some(ScoreEndState::Lose(depth));
     }
 
     if me.body[1..].contains(&me.body[0]) && depth != 0 {
-        return Some(SCORE_LOSE + depth);
+        return Some(ScoreEndState::Lose(depth));
     }
 
     if my_body.contains(&not_me.body[0]) {
-        return Some(SCORE_WIN - depth);
+        return Some(ScoreEndState::Win(depth));
     }
 
     if not_me.body[1..].contains(&not_me.body[0]) && depth != 0 {
-        return Some(SCORE_WIN - depth);
+        return Some(ScoreEndState::Win(depth));
     }
 
     let num_snakes: i64 = node.board.snakes.len().try_into().unwrap();
@@ -112,9 +144,9 @@ fn score(node: &GameState, depth: i64) -> Option<i64> {
 
     if me.body[0] == not_me.body[0] {
         if me.length > not_me.length {
-            return Some(SCORE_WIN - depth);
+            return Some(ScoreEndState::Win(depth));
         } else {
-            return Some(SCORE_LOSE + depth);
+            return Some(ScoreEndState::Lose(depth));
         }
     }
 
@@ -123,24 +155,26 @@ fn score(node: &GameState, depth: i64) -> Option<i64> {
             let l: i64 = me.body.len().try_into().unwrap();
             // TODO: Add the difference in lengts here so that I still try to grow even if I can't
             // grow enough to get bigger
-            return Some(
-                (l + 1000)
-                    - a_prime::shortest_distance(&node.board, &me.body[0], &node.board.food)
-                        .unwrap_or(500),
-            );
+            let closest_food_distance =
+                a_prime::shortest_distance(&node.board, &me.body[0], &node.board.food);
+
+            return Some(ScoreEndState::LongerThanOpponent(
+                depth,
+                closest_food_distance,
+            ));
         }
 
-        return Some(
-            2000 - a_prime::shortest_distance(&node.board, &me.body[0], &vec![not_me.body[0]])
-                .unwrap_or(666),
-        );
+        let distance_to_opponent =
+            a_prime::shortest_distance(&node.board, &me.body[0], &vec![not_me.body[0]]);
+
+        return Some(ScoreEndState::ShorterThanOpponent(
+            depth,
+            distance_to_opponent,
+        ));
     }
 
     None
 }
-
-const SCORE_LOSE: i64 = -10000;
-const SCORE_WIN: i64 = 10000;
 
 fn children(node: &GameState, turn_snake_id: &str) -> Vec<(Direction, Coordinate)> {
     let you: &Battlesnake = node
@@ -161,17 +195,23 @@ fn minimax(
     node: &mut GameState,
     depth: usize,
     is_maximizing: bool,
-    alpha: i64,
-    beta: i64,
+    alpha: ScoreEndState,
+    beta: ScoreEndState,
     current_moves: Vec<SnakeMove>,
-) -> (i64, Vec<SnakeMove>) {
+) -> (ScoreEndState, Vec<SnakeMove>) {
     let options = minimax_options(node, depth, is_maximizing, alpha, beta, current_moves);
 
-    let score_multiplier = if is_maximizing { 1 } else { -1 };
-    options
-        .into_iter()
-        .max_by_key(|(score, _)| score_multiplier * score.clone())
-        .unwrap_or((0, vec![]))
+    if is_maximizing {
+        options
+            .into_iter()
+            .max_by_key(|(score, _)| score.clone())
+            .unwrap()
+    } else {
+        options
+            .into_iter()
+            .min_by_key(|(score, _)| score.clone())
+            .unwrap()
+    }
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -186,10 +226,10 @@ fn minimax_options(
     node: &mut GameState,
     depth: usize,
     is_maximizing: bool,
-    alpha: i64,
-    beta: i64,
+    alpha: ScoreEndState,
+    beta: ScoreEndState,
     current_moves: Vec<SnakeMove>,
-) -> Vec<(i64, Vec<SnakeMove>)> {
+) -> Vec<(ScoreEndState, Vec<SnakeMove>)> {
     let mut alpha = alpha;
     let mut beta = beta;
 
@@ -198,7 +238,7 @@ fn minimax_options(
         return vec![(s, current_moves)];
     }
 
-    let mut options: Vec<(i64, Vec<SnakeMove>)> = vec![];
+    let mut options = vec![];
 
     if is_maximizing {
         let me = node.you.id.to_owned();
@@ -258,4 +298,36 @@ fn minimax_options(
     }
 
     options
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ScoreEndState::*;
+
+    #[test]
+    fn test_score_sorting() {
+        let mut sorted_vec = vec![
+            LongerThanOpponent(3, Some(4)),
+            LongerThanOpponent(3, Some(5)),
+            Lose(4),
+            Lose(7),
+            ShorterThanOpponent(4, None),
+            ShorterThanOpponent(4, Some(3)),
+            Win(5),
+        ];
+        sorted_vec.sort();
+
+        assert_eq!(
+            sorted_vec,
+            vec![
+                Lose(4),
+                Lose(7),
+                ShorterThanOpponent(4, None),
+                ShorterThanOpponent(4, Some(3)),
+                LongerThanOpponent(3, Some(4)),
+                LongerThanOpponent(3, Some(5)),
+                Win(5),
+            ]
+        )
+    }
 }
