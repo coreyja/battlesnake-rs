9bdfc628468b8f18d4b93919d3a9e7612951466b
Corey Alexander <coreyja@gmail.com>
2021-09-20 23:52:04 -0400

    Better a-prime start of game

--- battlesnake-rs/benches/a-prime.rs
+++ battlesnake-rs/benches/a-prime.rs
@@ -1,26 +1,27 @@
-use battlesnake_rs::a_prime::shortest_path;
-use battlesnake_rs::GameState;
+use battlesnake_rs::a_prime::*;
+use battlesnake_rs::*;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use pprof::criterion::{Output, PProfProfiler};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("a-prime start_of_game", |b| {
+    let mut g = c.benchmark_group("a-prime");
+    g.bench_function("wire start_of_game", |b| {
         let game_json = include_str!("../fixtures/start_of_game.json");
+        let game: GameState = serde_json::from_str(game_json).unwrap();
 
         b.iter(|| {
-            let game: GameState = serde_json::from_str(game_json).unwrap();
-            let game = black_box(game);
+            let game = black_box(&game);
             shortest_path(&game.board, &game.you.head, &game.board.food, None)
         })
     });
 
-    c.bench_function("a-prime a-prime-food-maze", |b| {
+    g.bench_function("wire a-prime-food-maze", |b| {
         let game_json = include_str!("../fixtures/a-prime-food-maze.json");
+        let game: GameState = serde_json::from_str(game_json).unwrap();
 
         b.iter(|| {
-            let game: GameState = serde_json::from_str(game_json).unwrap();
-            let game = black_box(game);
+            let game = black_box(&game);
             shortest_path(&game.board, &game.you.head, &game.board.food, None)
         })
     });
