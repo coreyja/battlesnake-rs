9f370cbb6a910aba9492b9a6ad96f1f29bdf667d
Corey Alexander <coreyja@gmail.com>
2022-03-11 19:47:26 -0500

    I forgot I need to copy this to wrapped

--- battlesnake-rs/src/flood_fill/spread_from_head.rs
+++ battlesnake-rs/src/flood_fill/spread_from_head.rs
@@ -211,40 +211,44 @@ impl<T: CellNum, const BOARD_SIZE: usize, const MAX_SNAKES: usize> SpreadFromHea
             sids
         };
 
-        let mut todo_per_snake: [TinyVec<
-            [Option<<Self as PositionGettableGame>::NativePositionType>; 4],
-        >; 4] = Default::default();
+        let mut todos: TinyVec<[CellWrapper<T>; 16]> = TinyVec::new();
+        let mut todos_per_snake: [u8; 4] = [0; 4];
 
         for sid in &sorted_snake_ids {
             for pos in self.get_snake_body_iter(sid) {
                 grid.cells[pos.as_usize()] = Some(*sid);
             }
+        }
 
+        for sid in &sorted_snake_ids {
             let head = self.get_head_as_native_position(sid);
-            todo_per_snake[sid.0 as usize].push(Some(head));
+            todos.push(CellWrapper(head));
+            todos_per_snake[sid.as_usize()] += 1;
         }
 
         for _ in 0..number_of_cycles {
-            for sid in &sorted_snake_ids {
-                let mut new_todo: TinyVec<
-                    [Option<<Self as PositionGettableGame>::NativePositionType>; 4],
-                > = Default::default();
+            let mut new_todos = TinyVec::new();
+            let mut new_todos_per_snake = [0; 4];
 
-                // Mark Neighbors
-                while let Some(pos) = todo_per_snake[sid.0 as usize].pop() {
-                    let pos =
-                        pos.expect("I forced everything into a Some so I could use a TinyVec here");
+            let mut todos_iter = todos.into_iter();
+
+            for sid in &sorted_snake_ids {
+                for _ in 0..todos_per_snake[sid.as_usize()] {
+                    // Mark Neighbors
+                    let pos = todos_iter.next().unwrap();
 
                     for neighbor in self.neighbors(&pos) {
                         if grid.cells[neighbor.as_usize()].is_none() {
                             grid.cells[neighbor.as_usize()] = Some(*sid);
-                            new_todo.push(Some(neighbor));
+                            new_todos.push(CellWrapper(neighbor));
+                            new_todos_per_snake[sid.as_usize()] += 1;
                         }
                     }
                 }
-
-                todo_per_snake[sid.0 as usize] = new_todo;
             }
+
+            todos = new_todos;
+            todos_per_snake = new_todos_per_snake;
         }
 
         grid
