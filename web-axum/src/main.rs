5a2080621b1c285bac335846ea9e786a1f3fd5eb
Corey Alexander <coreyja@gmail.com>
2022-04-24 13:00:05 -0400

    Extract BoxedFactory in Axum Extractor
    This is similiar to what we _wanted_ to do with Rocket a long time ago!
    
    We have a 'middleware' that can take a Requests and give us a
    `BoxedFactory` for the relevant snake. This makes it easy to define
    routes that automatically get the correct snake based on the pathing and
    does the right internals.
    
    This lets me keep the `BoxedFactory` interface I've always had without
    needing to manually wire up each Snake in Axum

--- web-axum/src/main.rs
+++ web-axum/src/main.rs
@@ -1,17 +1,40 @@
 #![deny(warnings)]
 
 use axum::{
-    error_handling::HandleError,
+    async_trait,
+    extract::{FromRequest, Path, RequestParts},
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
-use battlesnake_rs::{all_factories, Game};
+use battlesnake_rs::{all_factories, BoxedFactory, Game};
 
 use std::net::SocketAddr;
 
-use anyhow::Result;
+struct ExtractSnakeFactory(BoxedFactory);
+
+#[async_trait]
+impl<B> FromRequest<B> for ExtractSnakeFactory
+where
+    B: Send,
+{
+    type Rejection = (StatusCode, &'static str);
+
+    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
+        let Path(snake_name) = Path::<String>::from_request(req)
+            .await
+            .map_err(|_err| (StatusCode::NOT_FOUND, "Couldn't extract snake name"))?;
+
+        let factories = all_factories();
+        let factory = factories
+            .into_iter()
+            .find(|f| f.name() == snake_name)
+            .ok_or((StatusCode::NOT_FOUND, "No factory found"))?;
+
+        Ok(Self(factory))
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -23,16 +46,39 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/constant-carter", get(constant_carter_info))
-        .route("/constant-carter/start", post(constant_carter_start))
-        .route(
-            "/constant-carter/move",
-            post(HandleError::new(
-                tower::service_fn(constant_carter_move),
-                handle_anyhow_error,
-            )),
-        )
-        .route("/constant-carter/end", post(constant_carter_end));
+        .route("/:snake_name", get(route_info))
+        .route("/:snake_name/start", post(route_start))
+        .route("/:snake_name/move", post(constant_carter_move))
+        .route("/:snake_name/end", post(route_end));
+
+    // NOTE: Axum routes must start with a leading slash.
+    // This is NOT checked at compile time
+    //
+    // .route("/constant-carter", get(constant_carter_info))
+    // .route("/constant-carter/start", post(constant_carter_start))
+    // .route("/constant-carter/move", post(constant_carter_move))
+    // .route("/constant-carter/end", post(constant_carter_end));
+
+    // for factory in factories {
+    //     let wrapped = WrappedFactory::new(factory);
+
+    //     app = app.route(
+    //         &format!("/{}", wrapped.name()),
+    //         get(wrapped.make_info_route()),
+    //     );
+    //     app = app.route(
+    //         &format!("/{}/start", wrapped.name()),
+    //         post(constant_carter_start),
+    //     );
+    //     app = app.route(
+    //         &format!("/{}/move", wrapped.name()),
+    //         post(constant_carter_move),
+    //     );
+    //     app = app.route(
+    //         &format!("/{}/end", wrapped.name()),
+    //         post(constant_carter_end),
+    //     );
+    // }
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
@@ -46,51 +92,42 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-fn handle_anyhow_error(err: anyhow::Error) -> (StatusCode, String) {
-    (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        format!("Something went wrong: {}", err),
-    )
-}
+// fn handle_anyhow_error(err: anyhow::Error) -> (StatusCode, String) {
+//     (
+//         StatusCode::INTERNAL_SERVER_ERROR,
+//         format!("Something went wrong: {}", err),
+//     )
+// }
 
-async fn constant_carter_info() -> impl IntoResponse {
-    let factories = all_factories();
-    let carter_factory = factories
-        .iter()
-        .find(|f| f.name() == "constant-carter")
-        .unwrap();
-    let carter_info = carter_factory.about();
+async fn route_info(ExtractSnakeFactory(factory): ExtractSnakeFactory) -> impl IntoResponse {
+    let carter_info = factory.about();
 
     Json(carter_info)
 }
 
-fn constant_carter_move(Json(game): Json<Game>) -> Result<impl IntoResponse> {
-    let factories = all_factories();
-    let carter_factory = factories
-        .iter()
-        .find(|f| f.name() == "constant-carter")
-        .unwrap();
-    let carter = carter_factory.from_wire_game(game);
+async fn constant_carter_move(
+    ExtractSnakeFactory(factory): ExtractSnakeFactory,
+    Json(game): Json<Game>,
+) -> impl IntoResponse {
+    let snake = factory.from_wire_game(game);
 
-    Ok(Json(
-        carter
-            .make_move()
-            .map_err(|x| x.context("error making move"))?,
-    ))
+    let output = snake
+        .make_move()
+        .expect("TODO: We need to work on our error handling");
+
+    Json(output)
 }
 
-async fn constant_carter_start() -> impl IntoResponse {
+async fn route_start() -> impl IntoResponse {
     StatusCode::NO_CONTENT
 }
-async fn constant_carter_end(Json(game): Json<Game>) -> impl IntoResponse {
-    let factories = all_factories();
-    let carter_factory = factories
-        .iter()
-        .find(|f| f.name() == "constant-carter")
-        .unwrap();
-    let carter = carter_factory.from_wire_game(game);
+async fn route_end(
+    ExtractSnakeFactory(factory): ExtractSnakeFactory,
+    Json(game): Json<Game>,
+) -> impl IntoResponse {
+    let snake = factory.from_wire_game(game);
 
-    carter.end();
+    snake.end();
 
     StatusCode::NO_CONTENT
 }
