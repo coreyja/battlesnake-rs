c24a04a6570a5dec75e3c60cf5c2346b672b86e0
Corey Alexander <coreyja@gmail.com>
2022-06-16 20:27:45 -0400

    Need a few extra millis sometimes, go to 30 padding

--- battlesnake-rs/src/hovering_hobbs.rs
+++ battlesnake-rs/src/hovering_hobbs.rs
@@ -217,7 +217,7 @@ impl BattlesnakeFactory for Factory {
         let name = "hovering-hobbs";
 
         let options: SnakeOptions = SnakeOptions {
-            network_latency_padding: Duration::from_millis(20),
+            network_latency_padding: Duration::from_millis(30),
         };
 
         if game.is_arcade_maze_map() {
