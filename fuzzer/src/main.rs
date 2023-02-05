57d116a39a9cede4cc5e1aae7afc1a5080feecfe
Corey Alexander <coreyja@gmail.com>
2023-02-05 16:21:55 -0500

    Get fuzzer working nicely. It counts the number of turns now, we got to almost 20 million just now before I cancelled it

--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -15,42 +15,62 @@ use types::{
 
 mod fuzz;
 
-fn about_equal_snake(this: &BattleSnake, other: &Vec<Position>) -> bool {
+fn about_equal_snake(
+    this: &BattleSnake,
+    compact: &StandardCellBoard4Snakes11x11,
+    id_map: &HashMap<String, SnakeId>,
+) {
+    let id = this.id.clone();
+    let compact_id = id_map.get(&id).unwrap();
+    let other: Vec<Position> = compact
+        .get_snake_body_vec(&compact_id)
+        .into_iter()
+        .map(|c| c.into_position(11))
+        .collect();
+
+    assert_eq!(
+        this.body[0], this.head,
+        "Snake {} head is not at the front of the body from Go",
+        id
+    );
+
     let body_vec: Vec<_> = this.body.iter().cloned().collect();
-    &body_vec == other
+
+    assert_eq!(
+        this.head, other[0],
+        "Snake {id} first body piece does not match its head in Rust"
+    );
+
+    assert_eq!(body_vec, other, "Snake {id} body does not match");
 }
 
-fn about_equal(
+fn assert_about_equal(
     this: &Game,
     other: &StandardCellBoard4Snakes11x11,
     id_map: &HashMap<String, SnakeId>,
-) -> bool {
-    println!("Go:\n{}", this);
-    println!("Rust:\n{}", other);
+) {
+    // println!("Go:\n{}", this);
+    // println!("Rust:\n{}", other);
 
     let snake_ids = other.get_snake_ids();
 
-    this.board.height == other.get_height()
-        && this.board.width == other.get_width()
-        && this.board.snakes.len() == other.get_snake_ids().len()
-        && this.board.snakes.iter().all(|snake| {
-            let snake_id = id_map.get(&snake.id).unwrap();
-            snake.health == other.get_health(&snake_id) as i32
-                && snake.head == other.get_head_as_position(&snake_id)
-                && snake_ids.iter().any(|id| {
-                    let body = other
-                        .get_snake_body_iter(id)
-                        .map(|p| p.into_position(11))
-                        .collect();
-                    about_equal_snake(snake, &body)
-                })
-        })
+    assert_eq!(this.board.height, other.get_height());
+    assert_eq!(this.board.width, other.get_width());
+
+    assert_eq!(this.board.snakes.len(), snake_ids.len());
+
+    for snake in &this.board.snakes {
+        let snake_id = id_map.get(&snake.id).unwrap();
+
+        assert_eq!(snake.health, other.get_health(&snake_id) as i32);
+        about_equal_snake(&snake, &other, &id_map);
+    }
 }
 
 #[derive(Serialize, Debug, PartialEq)]
 struct SimulateMove {
     id: String,
-    r#move: Move,
+    r#move: String,
 }
 
 #[derive(Serialize, Debug, PartialEq)]
@@ -66,7 +86,10 @@ async fn official_simulate_moves(
 ) -> Result<Board> {
     let moves = moves
         .into_iter()
-        .map(|(id, r#move)| SimulateMove { id, r#move })
+        .map(|(id, r#move)| SimulateMove {
+            id,
+            r#move: r#move.to_string(),
+        })
         .collect();
     let body = SimulateRequest {
         game: orig.clone(),
@@ -85,50 +108,55 @@ async fn official_simulate_moves(
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    let mut game = fuzz::random_game();
-    let mut turns = 0;
     let mut rng = rand::thread_rng();
-
-    let you_id = game.you_id().to_owned();
-
     let client = reqwest::Client::new();
 
-    let id_map = build_snake_id_map(&game);
+    let mut rounds = 0;
 
-    while !game.is_over() {
-        let moves: Vec<_> = game
-            .random_reasonable_move_for_each_snake(&mut rng)
-            .collect();
+    loop {
+        let mut game = fuzz::random_game();
+        let id_map = build_snake_id_map(&game);
+        let you_id = game.you_id().to_owned();
 
-        println!("Before:\nMoves: {moves:?}\n{game}");
+        while !game.is_over() {
+            let moves: Vec<_> = game
+                .random_reasonable_move_for_each_snake(&mut rng)
+                .collect();
 
-        let official_result = official_simulate_moves(&client, &game, moves.clone())
-            .await
-            .unwrap();
+            // println!("Before:\nMoves: {moves:?}\n{game}");
 
-        // TODO: Compare with Rust
-        let compact =
-            StandardCellBoard4Snakes11x11::convert_from_game(game.clone(), &id_map).unwrap();
-        let compact_moves: Vec<(SnakeId, Vec<Move>)> = moves
-            .iter()
-            .map(|(id, m)| (*id_map.get(id).unwrap(), vec![*m]))
-            .collect();
-        let mut results = compact.simulate_with_moves(&Instrument {}, compact_moves);
-        let rust_result = results.next().unwrap().1;
+            let official_result = official_simulate_moves(&client, &game, moves.clone())
+                .await
+                .unwrap();
 
-        let mut fake_game = game.clone();
-        fake_game.board = official_result.clone();
-        assert!(about_equal(&fake_game, &rust_result, &id_map));
+            // TODO: Compare with Rust
+            let compact =
+                StandardCellBoard4Snakes11x11::convert_from_game(game.clone(), &id_map).unwrap();
+            let compact_moves: Vec<(SnakeId, Vec<Move>)> = moves
+                .iter()
+                .map(|(id, m)| (*id_map.get(id).unwrap(), vec![*m]))
+                .collect();
+            let mut results = compact.simulate_with_moves(&Instrument {}, compact_moves);
+            let rust_result = results.next().unwrap().1;
 
-        turns += 1;
-        game.board = official_result;
+            let mut fake_game = game.clone();
+            fake_game.board = official_result.clone();
+            assert_about_equal(&fake_game, &rust_result, &id_map);
 
-        let you = game.board.snakes.iter().find(|s| s.id == you_id);
+            rounds += 1;
+            game.board = official_result;
 
-        if let Some(you) = you {
-            game.you = you.clone();
-        } else {
-            game.you.health = 0;
+            let you = game.board.snakes.iter().find(|s| s.id == you_id);
+
+            if let Some(you) = you {
+                game.you = you.clone();
+            } else {
+                game.you.health = 0;
+            }
+
+            if rounds % 10_000 == 0 {
+                println!("Rounds: {}", rounds);
+            }
         }
     }
 
