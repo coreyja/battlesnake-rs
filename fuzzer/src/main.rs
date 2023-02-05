e672d4d3aeb7016a1fbb10d2eaa2b5ce03f1fd38
Corey Alexander <coreyja@gmail.com>
2023-02-05 12:33:18 -0500

    Get a loop started, need a offical server to hit to test

--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -1,6 +1,27 @@
+use battlesnake_rs::{Game, Move, RandomReasonableMovesGame, VictorDeterminableGame};
+
 mod fuzz;
 
-fn main() {
-    println!("Hello, world!");
-    dbg!(fuzz::random_game());
+async fn official_simulate_moves(orig: &Game, moves: Vec<(String, Move)>) -> Game {
+    todo!("official_simulate_moves")
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let mut game = fuzz::random_game();
+    let mut turns = 0;
+    let mut rng = rand::thread_rng();
+
+    while !game.is_over() {
+        println!("{}", fuzz::random_game());
+
+        let moves: Vec<_> = game
+            .random_reasonable_move_for_each_snake(&mut rng)
+            .collect();
+
+        let official_result = official_simulate_moves(&game, moves.clone()).await;
+
+        turns += 1;
+        game = official_result;
+    }
 }
